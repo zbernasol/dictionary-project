c663ed537724f5ffa084512bfbe850378abfc09e
Zian Bernasol <77581628+zbernasol@users.noreply.github.com>
2021-05-23 00:06:33 -0400

    Added Dictionary component and search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 import logo from "./logo.png";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo " alt="logo"  /> 
-       
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo " alt="logo"  /> 
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function search(event) {
+        event.preventDeault();
+        alert(`Searching for ${keyword} definition`);
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+    }
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+            </form>
+        </div>
+    );
+}
